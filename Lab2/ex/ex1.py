fbc4f6db71649517313a2d2abc8e8d175a830b0b
Enasel_Madalina_Bianca <madalina08be@gmail.com>
2022-10-04 19:04:43 +0300

    ex1 - done

--- Lab2/ex/ex1.py
+++ Lab2/ex/ex1.py
@@ -5,3 +5,15 @@ def gcd(a, b):
         return abs(a)
     else:
         return gcd(b, a % b)
+
+no_numbers= int(input("How many numbers are you planning on giving?"))
+numbers = [None]*no_numbers
+
+for i in range(no_numbers):
+    numbers[i] = int(input("What number would you like to give?"))
+
+finalGcd = gcd(numbers[0],numbers[1])
+for i in range(2,len(numbers)):
+    finalGcd= gcd(finalGcd,numbers[i])
+
+print(finalGcd)
