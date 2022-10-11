4399a581c6d1a0b04cfb240692e02be47cd9a4d6
Enasel_Madalina_Bianca <madalina08be@gmail.com>
2022-10-11 18:00:37 +0300

    ex5 - done

--- Lab2/ex/ex5.py
+++ Lab2/ex/ex5.py
@@ -1,52 +1,55 @@
 #Given a square matrix of characters writes a script that prints the string obtained by going
 # through the matrix in spiral order (as in the example):
 
-m = int(input("rows & columns: "))
-a = []
+R = int(input("Enter the number of rows:"))
+C = int(input("Enter the number of columns:"))
 
-for i in range(m):
-    row =[]
-    for j in range(m):
-        row.append((input('A[' + str(i) + ',' + str(j) +']: ')))
-    a.append(row)
+# Initialize matrix
+matrix = []
+print("Enter the entries rowwise:")
 
+# For user input
+for i in range(R):  # A for loop for row entries
+    a = []
+    for j in range(C):  # A for loop for column entries
+        a.append(int(input()))
+    matrix.append(a)
 
-def spiral_order(matrix):
-    ans = [] # matricea retunata,
-    if len(matrix) == 0:
-        return ans
-    l = len(matrix)
-    seen = [[0 for _ in range(l)] for _ in range(l)]
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
-    x = 0
-    y = 0
-    di = 0
-    # Iterate from 0 to R * C - 1
-    for i in range(l * l):
-        ans.append(matrix[x][y])
-        seen[x][y] = True
-        cr = x + dr[di]
-        cc = y + dc[di]
+def spiral_matrix_print(row, col, arr):
+    # Defining the boundaries of the matrix.
+    top = 0
+    bottom = row - 1
+    left = 0
+    right = col - 1
 
-        if (0 <= cr and cr < l and 0 <= cc and cc < l and not (seen[cr][cc])):
-            x = cr
-            y = cc
-        else:
-            di = (di + 1) % 4
-            x += dr[di]
-            y += dc[di]
-    return ans
+    direction = 0
 
+    while top <= bottom and left <= right:
+        if direction == 0:
+            for i in range(left, right + 1):  # moving left->right
+                print(arr[top][i], end=" ")
 
-# Driver code
-if __name__ == "__main__":
-    a = [[1, 2, 3, 4],
-         [5, 6, 7, 8],
-         [9, 10, 11, 12],
-         [13, 14, 15, 16]]
+            top += 1
+            direction = 1
 
-    # Function call
-    for x in spiral_order(a):
-        print(x, end=" ")
-    print()
+        elif direction == 1:
+            for i in range(top, bottom + 1):  # moving top->bottom
+                print(arr[i][right], end=" ")
+
+            right -= 1
+            direction = 2
+
+        elif direction == 2:
+            for i in range(right, left - 1, -1):  # moving right->left
+                print(arr[bottom][i], end=" ")
+
+            bottom -= 1
+            direction = 3
+
+        elif direction == 3:
+            for i in range(bottom, top - 1, -1):  # moving bottom->top
+                print(arr[i][left], end=" ")
+            left += 1
+            direction = 0
+
+spiral_matrix_print()(R,C, m)
